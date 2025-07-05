ac2ba42374cac0128509fca80046f71fb294c49b
alperenbugaz <alperenbugaz@gmail.com>
2025-07-05 14:31:42 +0300

    Add project files.

---
+++ Api/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, // Yayýmcýyý doðrula
+        ValidateAudience = true, // Hedefi doðrula
+        ValidateLifetime = true, // Token'ýn ömrünü kontrol et
+        ValidateIssuerSigningKey = true, // Ýmza anahtarýný doðrula
+        ValidIssuer = configuration["Jwt:Issuer"],
+        ValidAudience = configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+        ClockSkew = TimeSpan.Zero // Süre toleransýný sýfýrla
+    };
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
